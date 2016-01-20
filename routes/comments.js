72148449e980cf56481b58b91536cc2899e63cb1
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2016-01-20 13:10:08 -0700

    what

--- routes/comments.js
+++ routes/comments.js
@@ -7,22 +7,22 @@ function Comments() {
 }
 
 router.get('/:post_id/comments', function(req, res, next) {
+  console.log('&&&&&&&&&&&&&&&&');
+  console.log('&&&&&&&&&&&&&&&&');
   Comments().where('post_id', req.params.post_id).then(function (posts) {
     res.json({'SUCCESS': posts});
   })
 });
 
 router.post('/:post_id/comments', function(req, res, next) {
-  Comments().insert({commenter: req.body.commenter, body: req.body.body}).then(function (resp) {
-    res.redirect('/');
+  Comments().insert(req.body).then(function (resp) {
+    res.redirect('/posts/'+req.params.post_id+'/comments');
   })
 });
 
 router.get('/:post_id/comments/:comment_id', function(req, res, next) {
-  Comments().where('post_id', req.params.post_id).then(function (posts) {
-    Comments().where('id', req.params.comment_id).then(function (posts2) {
-      res.json({'SUCCESS': posts2});
-    })
+  Comments().where('id', req.params.comment_id).first().then(function (post) {
+    res.json({'SUCCESS': post});
   })
 });
 
@@ -36,19 +36,15 @@ router.get('/:post_id/comments/:comment_id/edit', function(req, res, next) {
 
 
 router.post('/:post_id/comments/:comment_id', function(req, res, next) {
-  Comments().where('post_id', req.params.post_id).then(function (posts) {
-    Comments().where('id', req.params.comment_id).update({commenter: req.body.commenter}).then(function (posts2) {
-      res.send(posts2)
-    })
+  Comments().where('id', req.params.comment_id).update({commenter: req.body.commenter}).then(function (posts2) {
+    res.redirect('/posts/'+req.params.post_id+'/comments');
   })
 });
 
-router.post('/:post_id/comments/:comment_id/del', function(req, res, next) {
-  Comments().where('post_id', req.params.post_id).then(function (posts) {
+router.post('/:post_id/comments/:comment_id/delete', function(req, res, next) {
     Comments().where('id', req.params.comment_id).del().then(function (posts2) {
-      res.send(posts2)
+      res.redirect('/posts/'+req.params.post_id+'/comments');
     })
-  })
 });
 
 module.exports = router;
